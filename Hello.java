d3016de129fc815d0fd3940d8d8c12d6bd96ceb8
sapna ragavi <jsapnaragavi@gmail.com>
2017-06-13 18:43:45 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Hello
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String str1=s.nextLine();
+        System.out.println(str1);
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Hello");
+        }
+    
+    }
+}
