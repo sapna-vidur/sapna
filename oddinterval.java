65ac7f285dbf8aa8f30c6acaf00d8a2ebaedf887
sapna ragavi <jsapnaragavi@gmail.com>
2017-06-18 03:01:42 -0700

    Create oddinterval.java

---
+++ oddinterval.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class oddintervals
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the starting range");
+        int a=s.nextInt();
+        System.out.println(a);
+        System.out.println("enter the ending range");
+        int b=s.nextInt();
+        System.out.println(b);
+        while(a<b)
+        {
+            if(a%2!=0)
+        {
+            System.out.println(a);
+        }
+         a=a+1;
+        }
+    }
+}
