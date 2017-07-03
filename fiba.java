244e88878dac27879b2d841bc15f7f4815cbb675
sapna ragavi <jsapnaragavi@gmail.com>
2017-07-02 23:22:16 -0700

    Create fiba.java

---
+++ fiba.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class fiba
+{
+   public static void main(String args[])  
+{   
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+ int n1=0,n2=1,n3,i;    
+ System.out.print(n1+" "+n2);  
+    
+ for(i=2;i<n;++i)   
+ {    
+  n3=n1+n2;    
+  System.out.print(" "+n3);    
+  n1=n2;    
+  n2=n3;    
+ }    
+  
+}}  
